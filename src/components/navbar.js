94a9392c2591004cdda47b40cfd6f37e88c8d423
Midplayz <88376046+Midplayz@users.noreply.github.com>
2024-12-28 01:40:15 +0530

    Added Navbar Support

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,33 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "gatsby";
 import "./navbar.css";
 
-const Navbar = () => (
-  <nav className="navbar">
-    <div className="navbar-logo">Srivishnu</div>
-    <ul className="navbar-links">
-      <li>
-        <Link to="/" activeClassName="active-link">
-          Home
-        </Link>
-      </li>
-      <li>
-        <Link to="/about" activeClassName="active-link">
-          About
-        </Link>
-      </li>
-      <li>
-        <Link to="/projects" activeClassName="active-link">
-          Projects
-        </Link>
-      </li>
-      <li>
-        <Link to="/resume" activeClassName="active-link">
-          Resume
-        </Link>
-      </li>
-    </ul>
-  </nav>
-);
+const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">Srivishnu</div>
+
+      <div className="burger-icon" onClick={toggleMenu}>
+        {menuOpen ? "✖" : "☰"} 
+      </div>
+
+      <ul className={`navbar-links ${menuOpen ? "mobile-menu" : ""}`}>
+        <li>
+          <Link to="/" activeClassName="active-link">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/about" activeClassName="active-link">
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to="/projects" activeClassName="active-link">
+            Projects
+          </Link>
+        </li>
+        <li>
+          <Link to="/resume" activeClassName="active-link">
+            Resume
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Navbar;
