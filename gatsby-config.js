d93d013465bca7de9f18970aef1e4aa131165829
Midplayz <88376046+Midplayz@users.noreply.github.com>
2024-12-28 23:10:12 +0530

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,12 @@
 /**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
- */
-
-/**
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
+  pathPrefix: "/Midplayz/Porfolio-website-ghp",
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Srivishnu's Portfolio`,
+    description: `Srivishnu's Portfolio which showcases all my projects and information you need to know me!`,
+    author: `@srivishnu`,
   },
   plugins: [
     `gatsby-plugin-image`,
@@ -27,7 +21,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `data`,
-        path: `${__dirname}/src/data/projects.json`, // Path to your JSON file
+        path: `${__dirname}/src/data/projects.json`,
       },
     },
     `gatsby-transformer-json`,
