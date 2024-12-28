1edb921d5c6357bf09855f7ab34f11a4b304867e
Midplayz <88376046+Midplayz@users.noreply.github.com>
2024-12-28 23:10:45 +0530

    Revert "Initial commit"
    This reverts commit d93d013465bca7de9f18970aef1e4aa131165829.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,18 @@
 /**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
+ */
+
+/**
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  pathPrefix: "/Midplayz/Porfolio-website-ghp",
   siteMetadata: {
-    title: `Srivishnu's Portfolio`,
-    description: `Srivishnu's Portfolio which showcases all my projects and information you need to know me!`,
-    author: `@srivishnu`,
+    title: `Gatsby Default Starter`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`,
+    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
     `gatsby-plugin-image`,
@@ -21,7 +27,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `data`,
-        path: `${__dirname}/src/data/projects.json`,
+        path: `${__dirname}/src/data/projects.json`, // Path to your JSON file
       },
     },
     `gatsby-transformer-json`,
