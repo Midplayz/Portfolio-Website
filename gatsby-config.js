8048a53bc15bdcc2551af9afe3d0feed04f02b82
Midplayz <88376046+Midplayz@users.noreply.github.com>
2024-12-28 23:12:44 +0530

    Changed Package Json stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,11 @@
 /**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
- */
-
-/**
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Srivishnu's Portfolio Website`,
+    description: `Srivishnu's Portfolio Website where all projects are showcased along with information about me.`,
+    author: `@srivishnu`,
   },
   plugins: [
     `gatsby-plugin-image`,
