68096bb4fd7b89f02fcd8a5695c2668478ecb459
Srivishnu Srinivasan <midnightassassinmc@gmail.com>
2025-01-14 13:04:20 +0530

    Update firebaseConfig.js

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDKA1QjwEcYayN5_Lz-wC1y_hTleEZ7cEI",
-    authDomain: "fitnesstracker-127f8.firebaseapp.com",
-    projectId: "fitnesstracker-127f8",
-    storageBucket: "fitnesstracker-127f8.appspot.com",
-    messagingSenderId: "120334602201",
-    appId: "1:120334602201:web:5717120668d324edbe53c8"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
 const app = initializeApp(firebaseConfig);
